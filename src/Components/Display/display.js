bf51695909712ac26f9c994df8f052b98db12a69
thiebautremy <remythiebaut@msn.com>
2023-02-24 15:30:30 +0100

    update styles and package

--- src/Components/Display/display.js
+++ src/Components/Display/display.js
@@ -1,65 +1,16 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import FormInput from '../../containers/formInput';
-import Statistiques from '../../containers/statistiques';
-import Particles from 'react-particles-js';
-import './display.scss';
+import React from "react";
+import PropTypes from "prop-types";
+import FormInput from "../../containers/formInput";
+import Statistiques from "../../containers/statistiques";
+import "./display.scss";
 
-const Display = ({isValide}) => {
-  return(
-    <div className="display">
-    <Particles
-    params={{
-	    "particles": {
-	        "number": {
-	            "value": 100,
-	            "density": {
-	                "enable": true,
-	                "value_area": 1500
-	            }
-	        },
-	        "line_linked": {
-	            "enable": false,
-	            "opacity": 0.02
-	        },
-	        "move": {
-	            "direction": "right",
-	            "speed": 0.05
-	        },
-	        "size": {
-	            "value": 2
-	        },
-	        "opacity": {
-	            "anim": {
-	                "enable": true,
-	                "speed": 1,
-	                "opacity_min": 0.05
-	            }
-	        }
-	    },
-	    "interactivity": {
-	        "events": {
-	            "onclick": {
-	                "enable": true,
-	                "mode": "push"
-	            }
-	        },
-	        "modes": {
-	            "push": {
-	                "particles_nb": 2
-	            }
-	        }
-	    },
-	    "retina_detect": true
-	}} 
-  className="display__particles"
-  />
-      {isValide? <Statistiques /> : <FormInput />} 
-    </div>
-  )
-}
+const Display = ({ isValide }) => {
+  return (
+    <div className="display">{isValide ? <Statistiques /> : <FormInput />}</div>
+  );
+};
 
 Display.propTypes = {
   isValide: PropTypes.bool.isRequired,
-}
-export default Display; 
+};
+export default Display;
