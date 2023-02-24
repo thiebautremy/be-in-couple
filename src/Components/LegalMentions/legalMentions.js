8b5ec1c8dd12e87592b6e8d6b9db6c6f552c80ca
thiebautremy <remythiebaut@msn.com>
2023-02-24 15:38:48 +0100

    update styles and package

--- src/Components/LegalMentions/legalMentions.js
+++ src/Components/LegalMentions/legalMentions.js
@@ -1,73 +1,39 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Particles from 'react-particles-js';
-import './legalMentions.scss';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./legalMentions.scss";
 
 const LegalMentions = () => (
-    <div className="legalMentions">
-    <Particles
-    params={{
-	    "particles": {
-	        "number": {
-	            "value": 100,
-	            "density": {
-	                "enable": true,
-	                "value_area": 1500
-	            }
-	        },
-	        "line_linked": {
-	            "enable": false,
-	            "opacity": 0.02
-	        },
-	        "move": {
-	            "direction": "right",
-	            "speed": 0.05
-	        },
-	        "size": {
-	            "value": 2
-	        },
-	        "opacity": {
-	            "anim": {
-	                "enable": true,
-	                "speed": 1,
-	                "opacity_min": 0.05
-	            }
-	        }
-	    },
-	    "interactivity": {
-	        "events": {
-	            "onclick": {
-	                "enable": true,
-	                "mode": "push"
-	            }
-	        },
-	        "modes": {
-	            "push": {
-	                "particles_nb": 2
-	            }
-	        }
-	    },
-	    "retina_detect": true
-	}} 
-  className="legalMentions__particles"
-  />
+  <div className="legalMentions">
     <article>
-    <h1>
-      Mentions légales
-    </h1>
-    <h2>Editeur :</h2>
+      <h1>Mentions légales</h1>
+      <h2>Editeur :</h2>
       <p>Le site Be in couple est édité par THIEBAUT Rémy</p>
-    <h2>Responsable de publication :</h2>
+      <h2>Responsable de publication :</h2>
       <p>THIEBAUT Rémy</p>
-    <h2>Traitement des données :</h2>
-      <p>Aucunes données n'est collectées. Les données rentrées par l'utilisateur sont supprimées dès le rafraichissement de la page.</p>
-      <p>Ce site est proposé en différents langages web (HTML5, Javascript, CSS, React, Redux ) pour un meilleur confort d’utilisation et un graphisme plus agréable, nous vous recommandons de recourir à des navigateurs modernes comme Google Chrome, Safari, Firefox, Edge, etc…</p>
-    <h2>Images</h2>
-      <p>Les images utilisées sont libre de droit est sont disponibles à ce lien : <a href='https://www.freepik.com/vectors/background'>Background vector created by brgfx - www.freepik.com</a></p>
-    </article>    
-      <Link to="/"><button type="button">Retour</button></Link>
-    </div>
-
+      <h2>Traitement des données :</h2>
+      <p>
+        Aucunes données n'est collectées. Les données rentrées par l'utilisateur
+        sont supprimées dès le rafraichissement de la page.
+      </p>
+      <p>
+        Ce site est proposé en différents langages web (HTML5, Javascript, CSS,
+        React, Redux ) pour un meilleur confort d’utilisation et un graphisme
+        plus agréable, nous vous recommandons de recourir à des navigateurs
+        modernes comme Google Chrome, Safari, Firefox, Edge, etc…
+      </p>
+      <h2>Images</h2>
+      <p>
+        Les images utilisées sont libre de droit est sont disponibles à ce lien
+        :{" "}
+        <a href="https://www.freepik.com/vectors/background">
+          Background vector created by brgfx - www.freepik.com
+        </a>
+      </p>
+    </article>
+    <Link to="/">
+      <button type="button">Retour</button>
+    </Link>
+  </div>
 );
 
 export default LegalMentions;
--- src/Components/Page404/page404.js
+++ src/Components/Page404/page404.js
@@ -1,74 +1,31 @@
-import React, {useState} from 'react';
-import { Redirect } from 'react-router-dom';
+import React, { useState } from "react";
+import { Redirect } from "react-router-dom";
 
-import Particles from 'react-particles-js';
-import './page404.scss';
+import "./page404.scss";
 
 const Page404 = () => {
-  
   const [redirect, setRedirect] = useState(false);
 
   return (
-  <div className="page404">
-    <Particles
-    params={{
-	    "particles": {
-	        "number": {
-	            "value": 100,
-	            "density": {
-	                "enable": true,
-	                "value_area": 1500
-	            }
-	        },
-	        "line_linked": {
-	            "enable": false,
-	            "opacity": 0.02
-	        },
-	        "move": {
-	            "direction": "right",
-	            "speed": 0.05
-	        },
-	        "size": {
-	            "value": 2
-	        },
-	        "opacity": {
-	            "anim": {
-	                "enable": true,
-	                "speed": 1,
-	                "opacity_min": 0.05
-	            }
-	        }
-	    },
-	    "interactivity": {
-	        "events": {
-	            "onclick": {
-	                "enable": true,
-	                "mode": "push"
-	            }
-	        },
-	        "modes": {
-	            "push": {
-	                "particles_nb": 2
-	            }
-	        }
-	    },
-	    "retina_detect": true
-	}} 
-  className="page404__particles"
-  />
-   <section className="page404__section">
-     <h1 className="page404__section__title">Vous ne vous seriez pas un peu perdu par hasard ???</h1>
-     <h1 className="page404__section__title">Allez...je vous aide à retourner à la page d'accueil :</h1>
-         <button 
-          type="button" 
+    <div className="page404">
+      <section className="page404__section">
+        <h1 className="page404__section__title">
+          Vous ne vous seriez pas un peu perdu par hasard ???
+        </h1>
+        <h1 className="page404__section__title">
+          Allez...je vous aide à retourner à la page d'accueil :
+        </h1>
+        <button
+          type="button"
           className="page404__section__btn"
           onClick={() => setRedirect(true)}
-          >
+        >
           Retour à l'accueil
-        </button> 
-   </section>
-   {redirect && <Redirect to ="/"/> }
-  </div>
-)};
+        </button>
+      </section>
+      {redirect && <Redirect to="/" />}
+    </div>
+  );
+};
 
 export default Page404;
